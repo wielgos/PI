4aab0ffb7fa7e1f2286f37ecd25697a68f0264d7
Mikolaj <wielgosmikolaj@gmail.com>
2021-05-18 12:34:12 +0200

    2x100 sol.c is broken kinda

--- week9/semi.c
+++ week9/semi.c
@@ -1,132 +1,28 @@
 #include <stdio.h>
-#include <malloc.h>
-#include <math.h>
 
-//        n-1      n-2      n-3            0
-// liczba 2^(n-1), 2^(n-2), 2^(n-3) .... 2^0
-//int *to_bin(int n, int m) {
-//    int *bin = calloc(n, sizeof(int));
-//    int s = n - 1;
-//    for (int i = n - 1; i >= 0; i--) {
-//        if (m >= pow(2, i)) {
-//            bin[i] = 1;
-//            m -= pow(2, i);
-//        }
-//    }
-//    return bin;
-//}
-
-int to_dec(int *bin, int n) {
-    int s = 0;
-    for (int i = n - 1; i >= 0; i--) {
-        if (bin[i] == 1) {
-            s += (int) pow(2, i);
-        }
+unsigned int check(int num, int n, int m){
+    if(num%m == 0){ //dont check num if its already an multiplier
+        return 0; //invalid
     }
-    return s;
-}
+    for (int i = 0; i<n; i++){ //we switch on n positions
+        num ^= 1 << i; //toggle switch(on/off) i-ty bit
+        if (num%m ==0 && num != 0) // 0modsmth would cause problems
+            return 1;
+        num ^= 1 << i; //back to previous state
+    }
+    return 0;
 
+}
 int main() {
     int n;
     int m;
-    int counter = 0;
+    int c = 0;
     scanf("%d %d", &n, &m);
-//    int *bin = to_bin(n, m);
-//    for(int i = n-1; i>=0; i--){
-//        printf("%d", bin[i]);
-//    }
-    int *bin = calloc(n, sizeof(int));
-    int *prevbin = calloc(n, sizeof(int));
-    int *results = calloc(1000000, sizeof(int));
-    int res_index = 0;
-    unsigned long long upper = (int) pow(2, n - 1);
-    int decimal;
-    while (1) {
-        //printf("basic:\n");
-//        for (int i = n - 1; i >= 0; i--) {
-//            printf("%d", bin[i]);
-//        }
-        //printf("\n");
-//        decimal = to_dec(bin, n);
-
-        //printf("check: %d\n", decimal);
-        //printf("dec:%d\n", to_dec(bin, n));
-//        if (decimal != 0 && m % decimal == 0 || decimal%m == 0) {
-//            int flag = 0;
-//            for (int u = 0; u < res_index; u++) {
-//                if (decimal == results[u])
-//                    flag = 1;
-//            }
-//            if (flag == 0) {
-//                counter++;
-//                //printf("found: %d\n", decimal);
-//                results[res_index] = decimal;
-//                res_index += 1;
-//            }
-//        }
-
-        for (int i = 0; i < n; i++) {
-            prevbin[i] = bin[i];
-        }
-        decimal = to_dec(bin, n);
-        //printf("checkdec:%d\n", decimal);
-        if (decimal != 0 && decimal % m != 0 && m % decimal != 0) {
-            for (int i = 0; i < n; i++) { //check for flips
-                //now we flip the 0 or 1
-                //printf("fliptime\n");
-                int comeback = bin[i];
-                if (bin[i] == 1)
-                    bin[i] = 0;
-                else
-                    bin[i] = 1;
-                //printf("binflipped: ");
-                for (int k = n - 1; k >= 0; k--) {
-                    //printf("%d", bin[k]);
-                }
-                //printf("\n");
-                decimal = to_dec(bin, n);
-                //printf("checkflipped: %d\n", decimal);
-                if (decimal != 0 && m % decimal == 0 || decimal % m == 0) {
-                    int flag = 0;
-                    for (int u = 0; u < res_index; u++) {
-                        if (decimal == results[u])
-                            flag = 1;
-                    }
-                    if (flag == 0) {
-                        counter++;
-                        //printf("found: %d\n", decimal);
-                        results[res_index] = decimal;
-                        res_index += 1;
-                    }
-                    bin[i] = comeback;
-                }
-            }
-
-            for (int i = 0; i < n; i++) { //comeback to previous form
-                bin[i] = prevbin[i];
-            }
-        }
-        //append binary \/\/\/\/ 1 time
-        for (int i = 0; i < 1; i++) {
-            if (bin[0] == 1) {
-                bin[0] = 0;
-                int s = 1;
-                while (bin[s] == 1) {
-                    bin[s] = 0;
-                    s++;
-
-                    if (s == n) {
-                        printf("%d", counter);
-                        return 0;
-                    }
-
-                }
-                bin[s] = 1;
-            } else
-                bin[0] = 1;
-        }
+    for (int i = 1; i<1<<n; i++){
+        if (check(i,n,m))
+            c++;
     }
-    free(bin);
-    free(prevbin);
+    printf("%d", c);
+
     return 0;
 }
--- week9/sol.c
+++ week9/sol.c
@@ -1,8 +1,75 @@
 #include <stdio.h>
 #include <malloc.h>
-#include <string.h>
 #include <math.h>
 
 int main() {
+    int n;
+    int g;
+    scanf("%d %d", &n, &g);
+    int *t = calloc(n,sizeof(int));
+    int max_len = 0;
+    for (int i =0; i<n; i++){
+        scanf("%d", &t[i]);
+        int len = floor(log2(t[i]) +1);
+        if (len > max_len)
+            max_len = len;
+    }
+    int *gbin = calloc(max_len,sizeof(int)); //liczba g binarnie
+    int m = g;
+    for (int j = max_len - 1; j >= 0; j--) {
+        if (m >= pow(2, j)) {
+            gbin[j] = 1;
+            m -= pow(2, j);
+        }
+    }//liczba g binarnie zbudowana
+
+    int *bins = calloc(n*max_len,sizeof(int));
+    for(int i =0; i<n; i++){
+        int m = t[i];
+//        if (m == g){
+//            m = 0;
+//            flag = 1;
+//        }
+
+        for (int j = max_len - 1; j >= 0; j--) {
+            if (m >= pow(2, j)) {
+                bins[i*max_len+j] = 1;
+                m -= pow(2, j);
+            }
+        }
+
+    }
+    int best_c = n+1;
+    for(int j =0; j<max_len; j++){
+        //i czyli pozycja gbina
+        int c = n+1;
+        if (gbin[j] == 1){
+            c = 0;
+            for(int i2 = 0; i2<n; i2++){
+                if (bins[i2*max_len +j] == 1)
+                    c++;
+            }
+        }
+        //printf("c:%d\n", c);
+        if (c < best_c)
+            best_c = c;
+
+    }
+    printf("%d\n", best_c);
+    printf("bins:\n");
+    for(int i =0; i<n;i++){
+        for(int j=max_len -1;j>=0; j--){
+            printf("%d", bins[i*max_len+j]);
+        }
+        printf("\n");
+    }
+    printf("gbin:\n");
+    for (int j = max_len - 1; j >= 0; j--) {
+        printf("%d", gbin[j]);
+    }
+
+//    if (flag)
+//        ;
+
     return 0;
 }
