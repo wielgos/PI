05107f1e469c638eaa80d4bdd67e8ad950a3bb0d
Mikolaj <wielgosmikolaj@gmail.com>
2021-05-19 13:29:20 +0200

    3x100 week9

--- week9/sol.c
+++ week9/sol.c
@@ -2,51 +2,71 @@
 #include <malloc.h>
 #include <math.h>
 
+int pow2(int k){
+    if (k == 0)
+        return 1;
+    return 2*pow2(k-1);
+}
+
+
 int main() {
     int n;
     int g;
     scanf("%d %d", &n, &g);
-    int *t = calloc(n,sizeof(int));
+    int *t = calloc(n, sizeof(int));
     int max_len = 0;
-    for (int i =0; i<n; i++){
+    int len;
+    for (int i = 0; i < n; i++) {
         scanf("%d", &t[i]);
-        int len = floor(log2(t[i]) +1);
+        len = (int)log2(t[i])+1;
         if (len > max_len)
             max_len = len;
     }
-    int *gbin = calloc(max_len,sizeof(int)); //liczba g binarnie
+    //printf("len:%d\n",max_len);
+    int *gbin = calloc(max_len, sizeof(int)); //liczba g binarnie
     int m = g;
     for (int j = max_len - 1; j >= 0; j--) {
-        if (m >= pow(2, j)) {
+        if (m >= pow2(j)) {
             gbin[j] = 1;
-            m -= pow(2, j);
+            m -= pow2(j);
         }
     }//liczba g binarnie zbudowana
-
-    int *bins = calloc(n*max_len,sizeof(int));
-    for(int i =0; i<n; i++){
-        int m = t[i];
-//        if (m == g){
-//            m = 0;
-//            flag = 1;
-//        }
-
+    int *bins = calloc(n * max_len, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        m = t[i];
         for (int j = max_len - 1; j >= 0; j--) {
-            if (m >= pow(2, j)) {
-                bins[i*max_len+j] = 1;
-                m -= pow(2, j);
+            if (m >= pow2(j)) {
+                bins[i * max_len + j] = 1;
+                m -= pow2(j);
             }
         }
-
     }
-    int best_c = n+1;
-    for(int j =0; j<max_len; j++){
-        //i czyli pozycja gbina
-        int c = n+1;
-        if (gbin[j] == 1){
+    //reset the wrong invalid bins
+    int fflag = 0;
+    for (int i = 0; i < n; i++) {
+        fflag = 0;
+        for (int j = max_len - 1; j >= 0; j--) {
+            //printf("%d %d",bins[i * max_len + j], gbin[j]);
+            if (bins[i * max_len + j] == 1 && gbin[j] == 0){
+                fflag = 1;
+                break;
+            }
+        }
+        if (fflag){
+            for (int j = max_len - 1; j >= 0; j--) {
+                //printf("flag");
+                bins[i * max_len + j] = 0;
+            }
+        }
+    }
+    int best_c = n + 1;
+    for (int j = 0; j < max_len; j++) {
+        //j czyli pozycja gbina
+        int c = n + 1;
+        if (gbin[j] == 1) {
             c = 0;
-            for(int i2 = 0; i2<n; i2++){
-                if (bins[i2*max_len +j] == 1)
+            for (int i2 = 0; i2 < n; i2++) {
+                if (bins[i2 * max_len + j] == 1)
                     c++;
             }
         }
@@ -56,10 +76,12 @@ int main() {
 
     }
     printf("%d\n", best_c);
+    return 0;
+
     printf("bins:\n");
-    for(int i =0; i<n;i++){
-        for(int j=max_len -1;j>=0; j--){
-            printf("%d", bins[i*max_len+j]);
+    for (int i = 0; i < n; i++) {
+        for (int j = max_len - 1; j >= 0; j--) {
+            printf("%d", bins[i * max_len + j]);
         }
         printf("\n");
     }
@@ -67,9 +89,5 @@ int main() {
     for (int j = max_len - 1; j >= 0; j--) {
         printf("%d", gbin[j]);
     }
-
-//    if (flag)
-//        ;
-
     return 0;
 }
