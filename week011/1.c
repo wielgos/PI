3d51cb02455842b72e615a8c65d42b23834c2ee6
Mikolaj <wielgosmikolaj@gmail.com>
2021-05-28 00:09:43 +0200

    templated

--- week011/1.c
+++ week011/1.c
@@ -4,121 +4,13 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define totalny_kox unsigned long long
-
-void append_num(int num[], int len) {
-    int remainder = 1;
-    int i = len - 1;
-    while (i >= 0 && remainder == 1) {
-        num[i] += 1;
-        num[i] = num[i] % 10;
-        if (num[i] != 0)
-            remainder = 0;
-    }
-}
-
-int modulo(int num[], int len) {
-    if (num[len - 1] % 2 == 1)
-        return 1;
-    else
-        return 0;
-}
-
-
-int divide_two(int num[], int len) {
-    int remainder = 0;
-    int new_remainder;
-    for (int i = 0; i < len; i++) {
-        new_remainder = (num[i] + remainder * 10) % 2;
-        num[i] = (num[i] + remainder * 10) / 2;
-        remainder = new_remainder;
-    }
-    return remainder;
-}
-
-
-int is_zero(int num[], int len) {
-    for (int i = 0; i < len; i++)
-        if (num[i] != 0)
-            return 0;
-    return 1;
-}
-
-int banf(int res[], int num[], int len, int m) {
-    int i = 0;
-    while (i < 100 && is_zero(num, len) == 0) {
-        res[i] = modulo(num, len);
-        if (i % 2 == m && res[i] == 1)
-            append_num(num, len);
-        divide_two(num, len);
-        i++;
-    }
-
-    if (is_zero(num, len) == 0)
-        return 0;
-    else
-        return 1;
-}
+#define ull unsigned long long
+#define PI 3.1415
+#define abs(a) (((a) < 0) ? -(a) : (a))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
 int main() {
-    // there is no "NO" case so I ignore that
-    // ^^ actually there is xddddd
-    // just a hunch forsenCD
-    int n, m, k;
-    int *t1 = calloc(40, sizeof(int));
-    int *t2 = calloc(40, sizeof(int));
-    char array[35];
-    scanf("%s", array);
-    //got array of the number
-    int num1[40];
-    int num2[40];
-    //printf("%s", array);
-    int len = strlen(array);
-    //len - leength of array
 
-    //printf("%d", len);
-
-    for (int i = 0; i < len; i++) {
-        num1[i] = array[i] - '0';
-        //printf("%d", num1[i]);
-    }
-
-    for (int i = 0; i < len; i++) {
-        num2[i] = array[i] - '0';
-        //printf("%d", num2[i]);
-    }
-
-    int result1[100] = {0};
-//    for(int i =0; i<100; i++){
-//        printf("%d", result1[i]);
-//    }
-    //result1,result2 filled with 0
-    int is_valid1;
-    is_valid1 = banf(result1, num1, len, 1);
-    if (is_valid1) {
-        for (int j = 0; j < 100; j++) {
-            if (result1[j] == 1) {
-                printf("%d ", j);
-                //printf("%d ", result1[j]);
-            }
-        }
-    } else
-        printf("NO");
-
-    printf("\n");
-    int is_valid2;
-    int result2[100] = {0};
-    is_valid2 = banf(result2, num2, len, 0);
-    if (is_valid2) {
-        for (int j = 0; j < 100; j++) {
-            if (result2[j] == 1) {
-                printf("%d ", j);
-                //printf("%d ", result2[j]);
-            }
-        }
-    } else
-        printf("NO");
-    free(t1);
-    free(t2);
     return 0;
 }
--- week011/2.c
+++ week011/2.c
@@ -1,135 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <math.h>
+#include <stdlib.h>
 
-#define OFFSET 50
-
-
-int get_offset(int i) {
-    return i - 100;
-}
-
-int set_offset(int i) {
-    return i + 100;
-}
-
-
-void plansza(int *t, int r, int l) {
-    for (int i = l; i <= r; i++) {
-        printf("%d ", t[i]);
-    }
-    printf("\n");
-    for (int i = l; i <= r; i++) {
-        printf("%d ", get_offset(i));
-    }
-    printf("\n-------------\n");
-}
-
-int check_end(int *t) {
-    for (int i = 0; i < 14999; i++) {
-        if (t[i] != 0 && t[i + 1] != 0) {
-            return 0;
-        }
-        if (t[i] > 1 || t[i + 1] > 1) {
-            return 0;
-        }
-    }
-    return 1;
-}
+#define ull unsigned long long
+#define PI 3.1415
+#define abs(a) (((a) < 0) ? -(a) : (a))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
 int main() {
-    int t[15000];
-    for (int i = 0; i < 15000; i++) {
-        t[i] = 0;
-    }
-    int n = 0;
 
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        int m = 0;
-        scanf("%d", &m);
-        m += OFFSET;
-        scanf("%d", &t[m]);
-    }
-    int size3 = 1;
-    //plansza(t,0,100);
-    while (size3) {
-        size3 = 0;
-        for (int i = 2; i < 14997; i++) {
-            if (t[i] >= 3) {
-                t[i - 2] += t[i] / 3;
-                t[i + 2] += t[i] / 3;
-                t[i] = t[i] % 3;
-                if (t[i - 2] >= 3) {
-                    size3 = 1;
-                }
-            }
-        }
-    }
-    int j = -1;
-    for (int i = 0; i < 14997; i++) {
-        if (t[i] != 0) {
-            j = i - 1;
-        }
-    }
-    //plansza(t,0,100);
-    while (check_end(t) == 0) {
-        int entered = 0;
-        //plansza(t,0,100);
-        if (t[j] > 0 && t[j + 1] > 0) {
-            //printf("enter:1\n");
-            //ruch w prawo
-            t[j] -= 1;
-            t[j + 1] -= 1;
-            t[j + 2] += 1;
-            j += 2;
-            entered = 1;
-        } else if (t[j] == 2 && t[j - 1] > 0) {
-            //printf("enter:2\n");
-            j -= 1;
-            entered = 1;
-        } else if (t[j] == 2 && t[j + 1] == 0) {
-            //printf("enter:3\n");
-            //ruch w prawo + lewo
-            t[j] -= 1;
-            t[j - 1] += 1;
-            t[j - 2] += 1;
-            t[j - 1] -= 1;
-            t[j] -= 1;
-            t[j + 1] += 1;
-            j += 1;
-            entered = 1;
-        } else if (t[j] == 3) {
-            //printf("enter:4\n");
-            //rozladuj
-            t[j - 2] += t[j] / 3;
-            t[j + 2] += t[j] / 3;
-            t[j] = t[j] % 3;
-            j += 2;
-            entered = 1;
-        }
-        int max_left = -1;
-        int left = -1;
-        for (int i = 0; i < 14997; i++) {
-            if (t[i] && max_left == -1) {
-                max_left = i;
-            }
-            if (t[i] && i < j) {
-                left = i;
-            }
-        }
-        if (j == max_left && entered == 0) {
-            //printf("enter:5\n");
-            break;
-        } else if (entered == 0) {
-            //printf("enter:6\n");
-            j = left;
-        }
-
-    }
-    //plansza(t,0,100);
-    for (int i = 0; i < 14997; i++) {
-        if (t[i] != 0) {
-            printf("%d ", i - OFFSET);
-        }
-    }
-    //plansza(t,0,100);
+    return 0;
 }
